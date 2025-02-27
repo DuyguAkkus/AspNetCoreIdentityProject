8d97d199d658c1633c9f2b62d9201e9b50c34285
Duygu_Akkuş <118392006+DuyguAkkus@users.noreply.github.com>
2025-02-27 13:12:40 +0300

    deneme

--- AspNetCoreIdentity.web/obj/Debug/net9.0/AspNetCoreIdentity.web.AssemblyInfo.cs
+++ AspNetCoreIdentity.web/obj/Debug/net9.0/AspNetCoreIdentity.web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspNetCoreIdentity.web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4a2388b494ebb707ee92952e93b565ee041a6be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b66453c761f3124d126c899368f8e6a8faebb08")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspNetCoreIdentity.web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspNetCoreIdentity.web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
